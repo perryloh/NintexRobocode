363bf8a6f1e9241fa1292f90e2751cb466c0d397
Perry Loh <perry.loh@gmail.com>
2013-11-20 16:24:18 +0800

    second commit

---
+++ Robo3/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace Nintex
+{
+    public class Myvi :Robot
+    {
+
+        public override void Run()
+        {
+            // -- Initialization of the robot --
+
+            // Here we turn the robot to point upwards, and move the gun 90 degrees
+            TurnRight(Heading - 90);
+            TurnGunLeft(90);
+
+            // Infinite loop making sure this robot runs till the end of the battle round
+            while (true)
+            {
+                // -- Commands that are repeated forever --
+
+                // Move our robot 5000 pixels ahead
+                Ahead(3000);
+
+                // Turn the robot 90 degrees
+                TurnRight(90);
+
+                // Our robot will move along the borders of the battle field
+                // by repeating the above two statements.
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            Fire(1);
+        }
+    }
+}
